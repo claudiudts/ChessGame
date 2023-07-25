fbbd81bb1e04de97d27b96d7633f27698d6cd6b9
Claudiu Sarb <sarb.claudiu391@gmail.com>
2023-07-25 00:43:52 -0500

    added some code
    added some code and starting to implement some logic for board

---
+++ ChessBoard.java
@@ -0,0 +1,13 @@
+public class ChessBoard {
+    private String[][] board;
+
+    public ChessBoard() {
+        board = new String[8][8];
+        initializeBoard();
+    }
+
+
+    private void initializeBoard() {
+
+    }
+}
